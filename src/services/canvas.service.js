5ee0793b5475bd08cecd0673c14e78c9b6c514cf
Seanoseanohay <lawrencekeener@gmail.com>
2025-10-15 12:52:18 -0700

    tmp project integration disabled for debug

--- src/services/canvas.service.js
+++ src/services/canvas.service.js
@@ -14,7 +14,11 @@ import {
 } from 'firebase/firestore'
 import { db, auth } from './firebase.js'
 import { FIREBASE_COLLECTIONS, OBJECT_UPDATE_THROTTLE } from '../constants/canvas.constants.js'
-import { canUserAccessProject } from './project.service.js'
+// TODO: Re-enable project integration after build issues are resolved
+// import { canUserAccessProject } from './project.service.js'
+
+// Temporary fallback function - allows all access for now
+const canUserAccessProject = async () => ({ success: true, canAccess: true });
 
 // Throttling mechanism for position updates during drag operations
 const pendingUpdates = new Map()
